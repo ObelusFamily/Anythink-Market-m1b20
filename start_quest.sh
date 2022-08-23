2365ee60a7a30bef591006b4426a1541e97d1436
Federico Mantovani <fettemanto@gmail.com>
2022-08-23 23:04:23 +0200

    Fixed bash issues in start_quest.sh

--- start_quest.sh
+++ start_quest.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 API_BASE_URL="${API_BASE_URL:="https://engine.wilco.gg"}"
-WILCO_ID="`cat .wilco 2> /dev/null`"
+WILCO_ID="$(cat .wilco 2>/dev/null)"
 
 sendEvent() {
   error=${2//$'\n'/\\n}
   body='{"event":"start_quest_script_ran", "metadata": {"success": '$1', "remote": false, "error": "'$error'"}}'
-  curl -m 5 -X POST $API_BASE_URL/users/$WILCO_ID/event -d "$body" -H 'Content-Type: application/json' &> /dev/null
+  curl -m 5 -X POST "$API_BASE_URL/users/$WILCO_ID/event" -d "$body" -H 'Content-Type: application/json' &>/dev/null
 }
 
 if [[ -z $WILCO_ID ]]; then
@@ -15,18 +15,17 @@ if [[ -z $WILCO_ID ]]; then
 fi
 
 printf "[=  ] Stashing any existing changes ... \n"
-git stash &> /dev/null
+git stash &>/dev/null
 
 printf "[== ] Resetting local 'main' branch to 'origin/main' ... \n"
-git_output=$((git checkout main && git fetch origin main && git reset --hard origin/main) 2>&1 > /dev/null)
+git_output=$( (git checkout main && git fetch origin main && git reset --hard origin/main) 2>&1 >/dev/null)
 git_exit_code=$?
 
-if ! test "$git_exit_code" -eq 0
-then
-   printf "[==X] ERROR: Could not reset local 'main' branch. Aborting.\n"
-   sendEvent false "$git_output"
-   echo "[==X] GIT ERROR: $git_output"
-   exit 1
+if ! test "$git_exit_code" -eq 0; then
+  printf "[==X] ERROR: Could not reset local 'main' branch. Aborting.\n"
+  sendEvent false "$git_output"
+  echo "[==X] GIT ERROR: $git_output"
+  exit 1
 fi
 
 printf "[===] SUCCESS: Your local 'main' branch is now up to date with origin.\n"
